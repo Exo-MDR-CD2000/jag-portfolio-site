e77090d465759068bb0516658e1769af4d926c15
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2025-01-09 16:45:19 -0700

    refactor Layout component to include Navbar and Footer; update structure for better layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SkipNavigation from './components/SkipNavigation';
-// import Navbar from './Navbar';
-// import Footer from './Footer';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 // import ScrollBackToTopButton from './ScrollBackToTop';
 
 const Layout: React.FC = () => (
-  <>
+  <div className='d-flex flex-column min-vh-100'>
     <SkipNavigation />
     <header role='banner'>
-      {/* <Navbar /> */}
+      <Navbar />
     </header>
     <main id='main-content' role='main'>
       <Outlet />
       {/* <ScrollBackToTopButton /> */}
     </main>
-    {/* <Footer /> */}
-  </>
+    <Footer />
+  </div>
 );
 
 export default Layout;
