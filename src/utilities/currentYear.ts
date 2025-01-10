7282a55dee8632ac0fde40a99f694f2614772094
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2025-01-09 17:30:31 -0700

    add helper function to return the current year for the footer

---
+++ src/utilities/currentYear.ts
@@ -0,0 +1,9 @@
+/**
+ * Helper function that returns the current year for the footer.
+ * @returns {number} The current year.
+ */
+const currentYear = () => {
+    return new Date().getFullYear();
+};
+
+export default currentYear;
