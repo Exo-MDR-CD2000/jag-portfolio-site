53fbeebe0682b5839cd8b8fe4bb3d62101815ad8
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2025-01-13 15:16:56 -0700

    add ProjectsPage component: create initial layout with PageTitle

---
+++ src/pages/ProjectsPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageTitle from "../components/PageTitle";
+import { Link } from "react-router-dom";
+
+const ProjectsPage: React.FC = () => {
+    return (
+        <>
+            <PageTitle title="Jose | Projects" />
+            <div className="container py-5">
+            </div>
+        </>
+    );
+};
+
+export default ProjectsPage;
